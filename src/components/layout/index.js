6ef9d18b30490789e3888301c8ba1d3805c5b969
Aja <aja.washington13@gmail.com>
2021-03-18 17:55:46 -0500

    git config corrected

---
+++ src/components/layout/index.js
@@ -0,0 +1,27 @@
+ md="mkdir"
+ th="touch"
+ bye="rm -r"
+ go2="cd ~/Workspace//NuNew"
+ cl="clear"
+ vs="code ."
+
+ zshconfig="mate ~/.zshrc"
+ ohmyzsh="mate ~/.oh-my-zsh"
+ ez="code ~/.zshrc"
+ eomz="code ~/.oh-my-zsh"
+ openz="open .zshrc"
+ sozsh="source ~/.zshrc"
+
+ gaa='git add --all'
+ gcom="git commit -m"
+ numain="git branch -M main"
+ 
+ gpo="git push origin"
+ gstat="git status"
+ gout="git checkout"
+ gcb='git checkout -b'
+ 
+ werk="yarn start"
+ nerd="npm run dev"
+ gpf!'='git push --force'
+ gm='git merge'
